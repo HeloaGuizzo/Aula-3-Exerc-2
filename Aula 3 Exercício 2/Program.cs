4b5f0babc2cad48f98b627dfa40f3c2e908d5e5c
HeloaGuizzo <107003060+HeloaGuizzo@users.noreply.github.com>
2022-06-13 19:14:27 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 3 Exercício 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int maior = 0;
+
+            Console.WriteLine("----- DETERMINAR O MAIOR VALOR -----");
+
+            Console.WriteLine("Informe o 1 valor: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            maior = n;
+
+            Console.WriteLine("Informe o 2 valor: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n > maior)
+            {
+                maior = n;
+            }
+
+            Console.WriteLine("Informe o 3 valor: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n > maior)
+            {
+                maior = n;
+            }
+
+            Console.WriteLine(" O maior valor informado é " + (maior));
+
+
+        }
+    }
+}
